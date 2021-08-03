55cbe0eed7d37e8f555abcb2c3828a2ee112d3e0
Ankit <ankit628792@gmail.com>
2021-08-03 12:04:08 +0530

    added logo

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -18,7 +18,7 @@ function Nav() {
 
     return (
         <div className={`nav ${show && "nav__black"}`}>
-            <img className="nav__logo" src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png" alt="Netflix Logo" />
+            <h1 className="nav__logo">NETFLIX</h1>
             <img className="nav__avatar" src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="" />
         </div>
     )
